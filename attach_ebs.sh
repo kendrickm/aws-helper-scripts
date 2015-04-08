86e0758e603280efa2d74b6502d8482ba875aac4
Kendrick Martin <kmartin@crowdcompass.com>
2015-04-08 14:41:50 -0700

    Changing variables

--- attach_ebs.sh
+++ attach_ebs.sh
@@ -1,3 +1,3 @@
 export AWS_ACCESS_KEY="AWSKEY"
-export AWS_SECRET_ACCESS_KEY="AWSSECRET"
-PATH=$PATH:$PWD/bin i-create-volume $1 none $2 $3 $4 $5 
+export AWS_SECRET_KEY="AWSSECRET"
+PATH=$PATH:$PWD/bin i-attach-volume $1 $2 $3
